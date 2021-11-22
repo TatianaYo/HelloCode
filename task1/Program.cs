648f9ba6fe2449dbb749c7df60ee8c8243d0583d
TatianaYo <yoienko@yandex.ru>
2021-11-22 20:59:59 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = new Random().Next(1, 100); // 1 2 3 4 ... 99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+
+if(numberB*numberB == numberA)
+{
+    Console.WriteLine("NumberA является квадратом numberB");
+}
+
+else
+{
+    Console.WriteLine("NumberA не является квадратом numberB");
+}
