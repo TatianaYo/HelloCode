973dcd8ab341db20fa0d331134d8b47cfb73c082
TatianaYo <yoienko@yandex.ru>
2021-11-22 21:21:07 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 7;
+int b = 9;
+
+int max = 0;
+int min = 0;
+
+if(a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a);
+}
