7cbc3ba415827b89114504b0a0f25607856dcae0
TatianaYo <yoienko@yandex.ru>
2021-11-25 15:09:58 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(1, 40); // 1 2 3 4 ... 39
+Console.Write("a = ");
+Console.WriteLine(a);
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("even integer");
+}
+else
+{
+    Console.WriteLine("uneven integer");
+}
