aad0d40d27fa21ff300ab83d0f604fc05a8f4dbb
TatianaYo <yoienko@yandex.ru>
2021-11-25 16:00:01 +0300

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(-40, -1);
+Console.Write("a = ");
+Console.WriteLine(a);
+int b = new Random().Next(1, 40);
+Console.Write("b = ");
+Console.WriteLine(b);
+
+while(a < b-1)
+{
+    Console.WriteLine(a+1);
+    a++;
+}
