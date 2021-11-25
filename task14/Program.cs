a6687f3f0a576d89b7ee5dd677268e10d11c7077
TatianaYo <yoienko@yandex.ru>
2021-11-25 22:19:32 +0300

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1, 999000);
+Console.Write("a = ");
+Console.WriteLine(a);
+
+if(a > 99)
+{
+    Console.Write("digit3 = ");
+    Console.Write((a / 100) % 10);
+}
+else
+{
+    Console.WriteLine("N/A");
+}
