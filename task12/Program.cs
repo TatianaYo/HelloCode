aa9dd9bb08741a39940c239485c23a4403f5fc98
TatianaYo <yoienko@yandex.ru>
2021-11-25 21:34:26 +0300

    task12

---
+++ task12/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Int32.Parse(Console.ReadLine());
+
+int digit1 = (a % 1000 - a % 100) / 100;
+int digit2 = (a % 100 - a % 10) / 10;
+int digit3 = a % 10;
+
+Console.Write(digit1);
+Console.Write(digit3);
