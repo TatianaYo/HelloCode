cbb5a3aadd26e10e2924faa81b2b3150decd15b0
TatianaYo <yoienko@yandex.ru>
2021-11-25 21:48:10 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int b = Int32.Parse(Console.ReadLine());
+int a = new Random().Next(1, 100);
+
+Console.Write("a = ");
+Console.WriteLine(a);
+if(a % b == 0)
+{
+   Console.WriteLine("Divisible");
+}
+else
+{
+    Console.WriteLine(a % b);
+}
