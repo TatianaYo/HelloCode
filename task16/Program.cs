b84969d1f153481aac38afc4aa6fba2a13a7101e
TatianaYo <yoienko@yandex.ru>
2021-11-29 20:12:07 +0300

    task16

---
+++ task16/Program.cs
@@ -0,0 +1,12 @@
+﻿int daynum = new Random().Next(1, 8);
+Console.Write("daynum = ");
+Console.WriteLine(daynum);
+
+if(daynum < 6)
+{
+    Console.WriteLine("Workday");
+}
+else
+{
+    Console.WriteLine("Weekend");
+}
