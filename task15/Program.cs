f222644669580a5cb5169d586a7a2c2418c3d688
TatianaYo <yoienko@yandex.ru>
2021-11-29 18:06:35 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int num = Int32.Parse(Console.ReadLine());
+
+if(num % 7 == 0)
+{
+    Console.WriteLine("Кратно 7");
+}
+else
+{
+    Console.WriteLine("Не кратно 7");
+}
+if( num % 23 == 0)
+{
+    Console.WriteLine("Кратно 23");
+}
+else
+{
+    Console.WriteLine("Не кратно 23");
+}
