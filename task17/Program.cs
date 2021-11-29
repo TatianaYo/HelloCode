62db816edd367d3b60931f6cd98c72dc58a9ca67
TatianaYo <yoienko@yandex.ru>
2021-11-29 20:23:32 +0300

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberB*numberB == numberA)
+{
+    Console.WriteLine("numberA является квадратом numberB");
+}
+else
+{
+    Console.WriteLine("numberA не является квадратом numberB");
+}
+if(numberA*numberA == numberB)
+{
+    Console.WriteLine("numberB является квадратом numberA");
+}
+else
+{
+    Console.WriteLine("numberB не является квадратом numberA");
+}
