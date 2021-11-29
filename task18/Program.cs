c3b412aec473e8d5da2fdbf559157c949756cec8
TatianaYo <yoienko@yandex.ru>
2021-11-30 00:39:13 +0300

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,18 @@
+﻿bool X = false;
+bool Y = true;
+
+bool F1 = !(X|Y);
+Console.Write("F1 = ");
+Console.WriteLine(F1);
+bool F2 = !X&!Y;
+Console.Write("F2 = ");
+Console.WriteLine(F2);
+
+if(F1==F2)
+{
+    Console.WriteLine("The expression is true");
+}
+else
+{
+    Console.WriteLine("The expression is false");
+}
