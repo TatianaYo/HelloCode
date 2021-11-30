db76e5835937947e491bb8f88101fa2203521c57
TatianaYo <yoienko@yandex.ru>
2021-11-30 18:51:20 +0300

    task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,28 +3,7 @@ int x = int.Parse(Console.ReadLine());
 Console.Write("Введите координату y: ");
 int y = int.Parse(Console.ReadLine());
 
-if(x > 0 || y > 0) 
-{
-   Console.WriteLine("1 quadrant"); 
-}
-else
-{
-    if(x < 0 || y > 0)
-    {
-    Console.WriteLine("2 quadrant");
-    }
-    else
-    {
-        if(x < 0 || y < 0)
-        {
-            Console.WriteLine("3 quadrant");
-        }
-        else
-        {
-            if(x > 0 || y < 0)
-            {
-                Console.WriteLine("4 quadrant");
-            }
-        }
-    }
-}
+if(x > 0 && y > 0) Console.WriteLine("1 quadrant"); 
+if(x < 0 && y > 0) Console.WriteLine("2 quadrant");
+if(x < 0 && y < 0) Console.WriteLine("3 quadrant");
+if(x > 0 && y < 0) Console.WriteLine("4 quadrant");
