27609b0e286ed236cb7a5fb9b07c4f0528689aea
TatianaYo <yoienko@yandex.ru>
2021-11-30 22:45:15 +0300

    task22.1

---
+++ task22/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти расстояние между точками в пространстве 2D/3D
+Console.Write("Введите координату xA: ");
+int xA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату yA: ");
+int yA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату xB: ");
+int xB = int.Parse(Console.ReadLine());
+Console.Write("Введите координату yB: ");
+int yB = int.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2));
+Console.Write("AB = ");
+Console.WriteLine(AB);
