d390204cb88e4b7199e14a23a95c1b50ed7c6d1a
TatianaYo <yoienko@yandex.ru>
2021-11-30 19:06:08 +0300

    task20

---
+++ task20/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Введите номер четверти: ");
+int quadrant = int.Parse(Console.ReadLine());
+
+if(quadrant == 1)
+{
+    Console.WriteLine("По оси X от 1 до бесконечности");
+    Console.WriteLine("По оси Y от 1 до бесконечности");
+}
+if(quadrant == 2)
+{
+    Console.WriteLine("По оси X от -1 до -бесконечности");
+    Console.WriteLine("По оси Y от 1 до бесконечности");
+}
+if(quadrant == 3)
+{
+    Console.WriteLine("По оси X от -1 до -бесконечности");
+    Console.WriteLine("По оси Y от -1 до -бесконечности");
+}
+if(quadrant == 4)
+{
+    Console.WriteLine("По оси X от 1 до бесконечности");
+    Console.WriteLine("По оси Y от -1 до -бесконечности");
+}
