99869cdfb966618ad50ddd5441cf3e516d1fff8a
TatianaYo <yoienko@yandex.ru>
2021-11-30 23:00:31 +0300

    task22

--- task22/Program.cs
+++ task22/Program.cs
@@ -1,13 +1,32 @@
-﻿//Найти расстояние между точками в пространстве 2D/3D
+﻿//Найти расстояние между точками в пространстве 2D
+//Console.Write("Введите координату xA: ");
+//int xA = int.Parse(Console.ReadLine());
+//Console.Write("Введите координату yA: ");
+//int yA = int.Parse(Console.ReadLine());
+//Console.Write("Введите координату xB: ");
+//int xB = int.Parse(Console.ReadLine());
+//Console.Write("Введите координату yB: ");
+//int yB = int.Parse(Console.ReadLine());
+
+//double AB = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2));
+//Console.Write("AB = ");
+//Console.WriteLine(AB);
+
+
+//Найти расстояние между точками в пространстве 3D
 Console.Write("Введите координату xA: ");
 int xA = int.Parse(Console.ReadLine());
 Console.Write("Введите координату yA: ");
 int yA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату zA: ");
+int zA = int.Parse(Console.ReadLine());
 Console.Write("Введите координату xB: ");
 int xB = int.Parse(Console.ReadLine());
 Console.Write("Введите координату yB: ");
 int yB = int.Parse(Console.ReadLine());
+Console.Write("Введите координату zB: ");
+int zB = int.Parse(Console.ReadLine());
 
-double AB = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2));
+double AB = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2) + Math.Pow(zB-zA, 2));
 Console.Write("AB = ");
 Console.WriteLine(AB);
