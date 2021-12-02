432b456ff6bf573489aff7a883fc05f8de6ccc79
TatianaYo <yoienko@yandex.ru>
2021-12-02 21:32:12 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,11 @@
+﻿//Подсчитать сумму цифр в числе
+Console.Write("Enter a number: ");
+int A = int.Parse(Console.ReadLine());
+int result = 0;
+
+while(A > 0)
+{
+    result = result + (A % 10);
+    A = A / 10;
+}
+Console.WriteLine(result);
