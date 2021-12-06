04012d58eece8c0f7fe7944b4c5cea20c3b38193
TatianaYo <yoienko@yandex.ru>
2021-12-06 21:28:20 +0300

    task37

---
+++ task37/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 1000);
+        //index = index + 1;
+        index++;
+    }
+}
+int[] array = new int[123];
+FillArray(array);
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i]>9 && array[i]<100)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine(count);
