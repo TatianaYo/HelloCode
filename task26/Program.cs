d3aa4f1a842a598076263b55aa9210ed5dcfb613
TatianaYo <yoienko@yandex.ru>
2021-12-06 18:24:27 +0300

    task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,7 +1,12 @@
 ﻿//Возведите число А в натуральную степень B используя цикл
 Console.Write("Введите число: ");
-int A = int.Parse(Console.WriteLine());
-int B = int.Parse(Console.WriteLine());
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
 int result = 1;
 
-for()
+for(int i = 0; i < B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine($"{A} в степени {B} = " + result);
