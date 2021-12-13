0b2a4ac05710224594252fa0795653b7c4282798
TatianaYo <yoienko@yandex.ru>
2021-12-13 19:45:45 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу копирования массива
+int[] array = {1,2,8,7,4};
+
+int[] arrayCopy = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+        arrayCopy[i] = array[i];
+        Console.Write(" " + arrayCopy[i]);
+}
